90aefab29adc7ae1549933a3efa897fdc632d8a0
maheshnehere <maheshnehere93@gmail.com>
2023-03-11 18:35:00 +0530

    Added:Employee attendance

---
+++ src/Bridgelabz/EmpAttendance.java
@@ -0,0 +1,19 @@
+package Bridgelabz;
+
+public class EmpAttendance {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        
+        int x = (int)(Math.random()*2);
+        System.out.println(x);
+
+        if (x == 1) {
+            System.out.println("Employee is present");
+        }
+
+        else{
+            System.out.println("Employee is Absent");
+        }
+    }
+    
+}
